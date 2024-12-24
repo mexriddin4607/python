667d79173404f3d4623805e636bb8d640b1e794a
mexriddin4607 <mexriddinaxmedov63@gamail.com>
2024-12-24 12:12:20 +0500

    bajarildi

---
+++ 22-misol.py
@@ -0,0 +1,8 @@
+narx = float(input("Mahsulot qiymatini kiriting: "))
+chegirma = float(input("Nechchi foiz chegirma bor: "))
+
+ayrish = (narx * chegirma) / 100
+
+chegirmali = narx -ayrish
+
+print(chegirmali)
